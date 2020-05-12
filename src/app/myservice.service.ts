77eab202191ede054c654e32ae94f3f83e18e781
KruthiRaveender <kruthi.16.11@gmail.com>
2020-05-12 20:03:07 +0530

    My commit Example

--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -10,14 +10,14 @@ export class MyserviceService {
   public getQuestions() {
     console.log("ins service get questions");
     const headers =new HttpHeaders().set('Content_Type', 'text/plain ;charset=utf-8');
-    return this.httpService.get<Question>("http://localhost:8653/questions/GetAllQuestions");
+    return this.httpService.get<Question>("http://localhost:5001/questions/GetAllQuestions");
   }
 
   public addQuestion(addQuestion: Question) {
     console.log("ins service add");
     console.log(addQuestion);
     const headers =new HttpHeaders().set('Content_Type', 'text/plain ;charset=utf-8');
-    return this.httpService.post("http://localhost:8653/questions/QuestionCreation", addQuestion,  { headers, responseType: 'text'});
+    return this.httpService.post("http://localhost:5000/questions/QuestionCreation", addQuestion,  { headers, responseType: 'text'});
   }
   
   public update(updateQuestion: Question) {
@@ -29,12 +29,12 @@ export class MyserviceService {
   public onUpdate(updateQuestion: Question) {
     console.log("ins service update");
     const headers =new HttpHeaders().set('Content_Type', 'text/plain ;charset=utf-8');
-    return this.httpService.put("http://localhost:8653/questions/UpdateQuestion", updateQuestion,  { headers, responseType: 'text'});
+    return this.httpService.put("http://localhost:5002/questions/UpdateQuestion", updateQuestion,  { headers, responseType: 'text'});
   }
   delete(id: number) {
     console.log("ins service delete");
     const headers =new HttpHeaders().set('Content_Type', 'text/plain ;charset=utf-8');
-    return this.httpService.delete("http://localhost:8653/questions/DeleteQuestion/" + id,  { headers, responseType: 'text'});
+    return this.httpService.delete("http://localhost:5003ng/questions/DeleteQuestion/" + id,  { headers, responseType: 'text'});
   }
 
 }
